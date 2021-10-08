00ef966a25a76274c3b3bea44843476a76b5e820
hxpmods <harpermusicidm@gmail.com>
2021-10-08 19:08:44 +0100

    A place to rest my weary potions
    -Made settings completely configurable
    -Added Solid bedside table

--- FlyingPotions.cs
+++ FlyingPotions.cs
@@ -1,6 +1,9 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using ObjectBased.Shelf;
 using System;
+using System.Linq;
 using UnityEngine;
 
 namespace FlyingPotions
@@ -13,11 +16,36 @@ namespace FlyingPotions
 		public const string pluginName = "Levitation Sensation";
 		public const string pluginVersion = "0.0.0.2";
 
+		//Mod config
+		public static ConfigEntry<bool> potionsFly;
+		public static ConfigEntry<float> flightGravity0;
+		public static ConfigEntry<float> flightGravity1;
+		public static ConfigEntry<float> flightGravity2;
+		public static ConfigEntry<float> flightGravity3;
+		public static ConfigEntry<float> flightGravity4;
+		public static ConfigEntry<float> flightGravity5;
+
+		public static ConfigEntry<bool> addShelf;
+
 		public void Awake()
 		{		
 			Debug.Log("Loading " + pluginName+", Version: " + pluginVersion);
 			DoPatching();
+			BindConfigs();
 			Debug.Log("Finished Loading " + pluginName + ", Version: " + pluginVersion);
+		}
+
+
+		private void BindConfigs()
+        {
+			potionsFly = Config.Bind("Levitation Station", "Potions Fly", true, "If your potions should fly or not.");
+			flightGravity0 = Config.Bind("Levitation Station", "Normal Gravity Amount", 1.0f, "Set gravity scale for all non-flight potions (only if above is currently active");
+			flightGravity1 = Config.Bind("Levitation Station", "Flight 1 Amount", 0.075f, "Set gravity scale for potions with Flight 1");
+			flightGravity2 = Config.Bind("Levitation Station", "Flight 2 Amount", 0f);
+			flightGravity3 = Config.Bind("Levitation Station", "Flight 3 Amount", -0.075f);
+			flightGravity4 = Config.Bind("Levitation Station", "Flight 4 Amount", -0.2f);
+			flightGravity5 = Config.Bind("Levitation Station", "Flight 5 Amount", -1.0f);
+			addShelf = Config.Bind("Levitation Station", "Solid bedside table", true, "Lets you place potions and ingredients on the beside table.");
 		}
 
 		private void DoPatching()
@@ -26,21 +54,50 @@ namespace FlyingPotions
 			harmony.PatchAll();
 		}
 
-		//Patching start
 
-		[HarmonyPatch(typeof(ItemFromInventory))]
-		[HarmonyPatch("OnReleasePrimary")]
-		class FlightPotionsFlyPatch
+		public static void AddMagicShelf()
+        {
+			//var shelf = Shelf.shelves.FirstOrDefault();
+			//Debug.Log(shelf);
+			//var shelf = GameObject.Find("Room Lab");
+
+			//get shelf
+			Shelf shelf = Resources.FindObjectsOfTypeAll<Shelf>().First();
+
+			//Copy shelf at new position
+			shelf = Instantiate(shelf, new Vector3(4.5f,12.2f,0),new Quaternion());
+
+			//Get sprite and turn it off
+			SpriteRenderer sr = shelf.gameObject.GetComponentInChildren<SpriteRenderer>();
+			sr.enabled = false;
+		}
+
+
+	}
+
+	[HarmonyPatch(typeof(ItemFromInventory))]
+	[HarmonyPatch("OnReleasePrimary")]
+	class FlightPotionsFlyPatch
+	{
+		static void Postfix(ItemFromInventory __instance)
 		{
-			static void Postfix(ItemFromInventory __instance)
-			{
-				PotionItem potionItem = __instance as PotionItem;
+
+
+			if(FlyingPotions.potionsFly.Value)
+            {
+
+			PotionItem potionItem = __instance as PotionItem;
 
 				if (potionItem != null)
 				{
 					//Access this array according to the amount of flying effects in the released potion.
 					//0 flying effects = 1.0f = default gravity.
-					float[] gravityValues = { 1.0f, 0.075F, 0F, -0.075F,-0.2f,-1.0f };
+					float[] gravityValues = { FlyingPotions.flightGravity0.Value,
+										FlyingPotions.flightGravity1.Value,
+										FlyingPotions.flightGravity2.Value,
+										FlyingPotions.flightGravity3.Value,
+										FlyingPotions.flightGravity4.Value,
+										FlyingPotions.flightGravity5.Value};
 
 					Potion potion = potionItem.inventoryItem as Potion;
 
@@ -63,10 +120,26 @@ namespace FlyingPotions
 						}
 
 					}
+
 				}
 			}
-
 		}
 
 	}
+
+	[HarmonyPatch(typeof(RoomManager))]
+	[HarmonyPatch("OrderedStart")]
+	class AddObjectsPatch
+    {
+		static void Postfix()
+        {
+			if (FlyingPotions.addShelf.Value) {
+				Debug.Log("Attempting to add Shelf");
+				FlyingPotions.AddMagicShelf();
+			}
+        }
+    }
+
+
+
 }
